604d0c33cd6c54e3d2bcdef12deebf7d5af41e52
parthmanhas <parthmanhas@gmail.com>
2025-02-19 16:38:20 +0530

    refactor: remove unnecessary useEffect import from ExpandableHabit component

--- src/components/ExpandableHabit.tsx
+++ src/components/ExpandableHabit.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { HabitTracker } from './HabitTracker';
 import { cn } from '@/lib/utils';
 import { CheckCircle2, ChevronDown, ChevronUp, Circle } from 'lucide-react';
